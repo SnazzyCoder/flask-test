c6bafa2d148c24028cd7273c60df8f915da577dc
Mohit Yadav <www.mohit2004@gmail.com>
2021-01-28 20:09:16 +0530

    Twitter mods

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app=Flask(__name__)
 def home():
     if request.method=='POST':
         # Handle POST Request here
-        return render_template('index.html')
-    return render_template('index.html')
+        return render_template('homw.html')
+    return render_template('homw.html')
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
