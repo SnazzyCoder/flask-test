06ea14498f8b459b7d5276cd1f6d804ee0061af3
Mohit Yadav <www.mohit2004@gmail.com>
2021-01-29 00:10:22 +0530

    major change

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
 from flask import Flask,redirect,url_for,render_template,request
+from flaskext.mysql import MySQL
+
 
 app=Flask(__name__)
+
+app.config['MYSQL_DATABASE_HOST'] = "remotemysql.com"
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_USER'] = "ggAVIcwN9H"
+app.config['MYSQL_DATABASE_PASSWORD'] = "dEl1QjNO73"
+app.config['MYSQL_DATABASE_DB'] = "ggAVIcwN9H"
+mysql = MySQL()
+mysql.init_app(app)
+
+
 @app.route('/',methods=['GET','POST'])
 def home():
     if request.method=='POST':
         # Handle POST Request here
-        return render_template('home.html')
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        
+        try: 
+            cursor.execute(f"INSERT INTO tweets (user_id, tweet_content) values ('{random.randint(100000000, 999999999)}', '{request.form['tweet']}')")
+            success = True
+        except Exception as e:
+            print("!!!!   error : ", e)
+        finally: 
+            conn.close()
+        
+        try: success
+        except: success = False
+        
+        return render_template('home.html', tweet_sucess=success)
     return render_template('home.html')
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PRODUCTION
-    app.run(port=5000,debug=True)
+    app.run(port=5000, debug=True)
